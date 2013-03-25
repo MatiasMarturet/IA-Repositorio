c20b0cb7eab494547b856e304f9bed133df4a197
Matias <matiasmarturet@hotmail.com>
2013-03-25 01:27:13 -0300

    Probando... Cracion de un tablero de ajedrez

---
+++ IA/src/entornoVisual/VentanaPrincipal.java
@@ -0,0 +1,86 @@
+package entornoVisual;
+
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JInternalFrame;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class VentanaPrincipal {
+
+	private JFrame frame;
+	JPanel squares[][] = new JPanel[8][8];
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal window = new VentanaPrincipal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public VentanaPrincipal() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 636, 526);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmCreateBoard = new JMenuItem("Create board");
+		mnMenu.add(mntmCreateBoard);
+		
+		//Inicio de creacion del tablero
+		final JInternalFrame internalFrame = new JInternalFrame("Board");
+		frame.getContentPane().add(internalFrame, BorderLayout.CENTER);
+		internalFrame.setVisible(false);
+		internalFrame.getContentPane().setLayout(new GridLayout(8, 8));
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				squares[i][j] = new JPanel();
+				if ((i + j) % 2 == 0) {
+					squares[i][j].setBackground(Color.black);
+				} else {
+					squares[i][j].setBackground(Color.white);
+				}   
+				internalFrame.getContentPane().add(squares[i][j]);
+			}
+		}
+				
+		internalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		internalFrame.setVisible(false);
+			
+		//Fin de creacion del tablero
+		
+		mntmCreateBoard.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				internalFrame.setVisible(true);
+			}
+		});
+		
+	}
+
+}
